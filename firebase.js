48ec83998dc35856e54ddee23d4682fd5741f0ae
Maxwell-T-Chikoti <161644053+Maxwell-T-Chikoti@users.noreply.github.com>
2024-10-16 22:06:34 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCDKZtayK_jYblFn5MAoInE-NgZRlBd5-g",
-  authDomain: "recipehub-b764e.firebaseapp.com",
-  projectId: "recipehub-b764e",
-  storageBucket: "recipehub-b764e.appspot.com",
-  messagingSenderId: "609011871120",
-  appId: "1:609011871120:web:fb708565526912fb639715"
+  apiKey: ""
+  authDomain: ".",
+  projectId: "",
+  storageBucket:  "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
